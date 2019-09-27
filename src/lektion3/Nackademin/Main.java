0cfb1f0981d65d5d1f4491f26bde5501842e5c13
Linus Svahn <respons29@gmail.com>
2019-09-27 10:52:27 +0200

    ADDED MORE FEATURES

--- src/lektion3/Nackademin/Main.java
+++ src/lektion3/Nackademin/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test för git hantering");
         System.out.println("BONUS FEATURES");
+        System.out.println("MORE FEATURES");
     }
 }
